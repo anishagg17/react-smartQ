75592459b57d71638e77f6f16ff19a977ab84d00
Anish Aggarwal <anish17122000@gmail.com>
2019-11-18 20:14:55 +0530

    Updates

---
+++ precache-manifest.a7b4d1b86635bd2671b351032e7e3236.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608c6e8dde1f6c30b906c09ef27d712f",
+    "url": "/react-smartQ/index.html"
+  },
+  {
+    "revision": "5d9e620f6790fbff236d",
+    "url": "/react-smartQ/static/css/main.212fd71c.chunk.css"
+  },
+  {
+    "revision": "3d5e7061cb6998a0ba13",
+    "url": "/react-smartQ/static/js/2.a8aab4c3.chunk.js"
+  },
+  {
+    "revision": "5d9e620f6790fbff236d",
+    "url": "/react-smartQ/static/js/main.38d127ca.chunk.js"
+  },
+  {
+    "revision": "1fe4560006ad477f8cf8",
+    "url": "/react-smartQ/static/js/runtime-main.16d88d77.js"
+  }
+]);
